063c4548cd86a8efbe753b75c15bb306306d62d2
jbride <jbride2001@yahoo.com>
2024-10-15 12:48:49 -0400

    bitcoin test

--- examples/weather_client.rs
+++ examples/weather_client.rs
@@ -1,6 +1,8 @@
 mod async_helpers;
 
 use std::error::Error;
+use std::fs;
+use chrono;
 
 use zeromq::{Socket, SocketRecv};
 
@@ -8,16 +10,20 @@ use zeromq::{Socket, SocketRecv};
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut socket = zeromq::SubSocket::new();
     socket
-        .connect("tcp://127.0.0.1:5556")
+        .connect("tcp://127.0.0.1:29001")
         .await
         .expect("Failed to connect");
 
     socket.subscribe("").await?;
 
-    for i in 0..10 {
+    let mut i = 0;
+    loop {
         println!("Message {}", i);
         let repl = socket.recv().await?;
-        dbg!(repl);
+        //dbg!(repl);
+        let path = format!("/tmp/{:?}", chrono::offset::Local::now());
+        fs::write(path, repl).expect("unable to write to file");
+        i += 1;
     }
     Ok(())
 }
